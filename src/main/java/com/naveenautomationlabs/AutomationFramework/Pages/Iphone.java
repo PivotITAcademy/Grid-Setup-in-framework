1f3c63194accc92b20ed4e3573728c142644ef57
Kamaldeep2023 <kamal.jammu100@gmail.com>
2023-07-09 14:37:33 -0400

    first commit

--- src/main/java/com/naveenautomationlabs/AutomationFramework/Pages/Iphone.java
+++ src/main/java/com/naveenautomationlabs/AutomationFramework/Pages/Iphone.java
@@ -3,3 +3,20 @@ package com.naveenautomationlabs.AutomationFramework.Pages;
 public class Iphone {
 
 }
+
+
+//private YourStore yourStore;
+
+	/*
+	 * @BeforeMethod public void setUp() { intialization(); yourStore = new
+	 * YourStore();
+	 * 
+	 * }
+	 * 
+	 * @Test public void validateProductSelected() {
+	 * yourStore.selectProductFromRow("iPhone");
+	 * 
+	 * }
+	 * 
+	 * @AfterMethod public void tearDown() { // wd.close(); }
+	 */
