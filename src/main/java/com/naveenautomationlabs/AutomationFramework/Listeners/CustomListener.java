6572371ad06a76972e2372fab03b1e3d47221442
Kamaldeep2023 <kamal.jammu100@gmail.com>
2023-07-09 14:06:57 -0400

    first commit

--- src/main/java/com/naveenautomationlabs/AutomationFramework/Listeners/CustomListener.java
+++ src/main/java/com/naveenautomationlabs/AutomationFramework/Listeners/CustomListener.java
@@ -1,52 +1,53 @@
 package com.naveenautomationlabs.AutomationFramework.Listeners;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import com.naveenautomationlabs.AutomationFramework.Utils.Utils;
 import com.naveenautomationlabs.AutomationFramework.base.TestBase;
 
-public class CustomListener extends TestBase implements ITestListener{
+public class CustomListener extends TestBase implements ITestListener {
 
 	@Override
-	 public void onTestStart(ITestResult result) 
-	{
+	public void onTestStart(ITestResult result) {
 		logger.info("Test has started " + result.getMethod().getMethodName() + "  ***********");
 	}
+
 	@Override
-	 public void onTestSuccess(ITestResult result) 
-	{
+	public void onTestSuccess(ITestResult result) {
 		logger.info("Test" + result.getMethod().getMethodName() + "Passed");
-		
+
 	}
-	
+
 	@Override
-	public void onTestFailure(ITestResult result) 
-	{
+	public void onTestFailure(ITestResult result) {
 		logger.info("Test " + result.getMethod().getMethodName() + "   Failed" + "!!!!!!!!!!!!!!!!!!!!");
 		Utils.takeScreenShot(result.getMethod().getMethodName());
 	}
-	
+
 	@Override
-	public void onTestSkipped(ITestResult result) 
-	{
+	public void onTestSkipped(ITestResult result) {
 		logger.info("Test " + result.getMethod().getMethodName() + "  Skipped" + "#################");
 	}
+
 	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
 		
 	}
-	/*
-	 * @Override public void onStart(ITestContext context) { // TODO Auto-generated
-	 * method stub
-	 * 
-	 * }
-	 * 
-	 * @Override public void onFinish(ITestContext context) { // TODO Auto-generated
-	 * method stub
-	 * 
-	 * }
-	 */
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
