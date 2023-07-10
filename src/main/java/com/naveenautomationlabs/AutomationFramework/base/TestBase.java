d1da86d092199804a8fc81aaf536d762f758318e
Kamaldeep2023 <kamal.jammu100@gmail.com>
2023-07-10 09:15:08 -0400

    first commit

--- src/main/java/com/naveenautomationlabs/AutomationFramework/base/TestBase.java
+++ src/main/java/com/naveenautomationlabs/AutomationFramework/base/TestBase.java
@@ -63,8 +63,8 @@ public class TestBase {
 	public void intialization()
 	{
 		
-	//	String browserName= prop.getProperty("browser");	
-		switch (browserName.getBrowserName()) {
+		String browserName= System.getProperty("Browser");	
+		switch (browserName) {
 		case "Chrome":
 			wd = WebDriverManager.chromedriver().create();// this will return chromedriver and create method returns webdriver instance
 			break;
